94623076d78a6d5088aa9da1721ba19375487daf
Ivan <ichang@broadinstitute.org>
2020-10-27 08:35:06 -0400

    Clean up group, version. Add artifactory publishing.

---
+++ src/main/java/bio/terra/testrunner/plugin/EchoPluginExtension.java
@@ -0,0 +1,13 @@
+package bio.terra.testrunner.plugin;
+
+public class EchoPluginExtension {
+  String in = "echo";
+
+  public String getIn() {
+    return in;
+  }
+
+  public void setIn(String in) {
+    this.in = in;
+  }
+}
