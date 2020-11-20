d8d7fbd4d7e156163eb8a5ff6473ca0125b1aa40
Mariko Medlock <mariko.medlock@gmail.com>
2020-11-20 17:43:18 -0500

    Add all TestRunner tasks to TestRunner plugin.

--- src/main/java/bio/terra/testrunner/plugin/TestRunnerPlugin.java
+++ src/main/java/bio/terra/testrunner/plugin/TestRunnerPlugin.java
@@ -1,13 +1,72 @@
 package bio.terra.testrunner.plugin;
 
+import bio.terra.testrunner.common.commands.CollectMeasurements;
+import bio.terra.testrunner.common.commands.LockAndRunTest;
+import bio.terra.testrunner.common.commands.LockNamespace;
+import bio.terra.testrunner.common.commands.PrintHelp;
+import bio.terra.testrunner.common.commands.RunTest;
+import bio.terra.testrunner.common.commands.UnlockNamespace;
+import bio.terra.testrunner.common.commands.UploadResults;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.tasks.SourceSetContainer;
+import org.gradle.api.tasks.TaskContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TestRunnerPlugin implements Plugin<Project> {
+  private static final Logger logger = LoggerFactory.getLogger(TestRunnerPlugin.class);
+
+  private static final String PROPERTY_NAME_SOURCE_SETS = "sourceSets";
+  private static final String SOURCE_SET_NAME_MAIN = "main";
+
   @Override
   public void apply(Project project) {
-    TestRunnerPluginExtension extension =
-        project.getExtensions().create("testrunner", TestRunnerPluginExtension.class);
-    TestRunnerTask testRunnerTask = project.getTasks().create("testrunner", TestRunnerTask.class);
+    // create a default version of the extension = properties relevant to >1 task
+    // these values can be overridden by defining them in the build.gradle file
+    logger.debug("Adding the TestRunner plugin extension to {}", project.getName());
+    project
+        .getExtensions()
+        .create(TestRunnerPluginExtension.EXTENSION_NAME, TestRunnerPluginExtension.class);
+
+    // apply the Java plugin, so that the JavaExec task will work
+    logger.debug("Applying the Java plugin to {}", project.getName());
+    project.getPlugins().apply(JavaPlugin.class);
+
+    // create the tasks
+    logger.debug("Adding the TestRunner tasks to {}", project.getName());
+    Class[] commandClasses = {
+      CollectMeasurements.class,
+      LockAndRunTest.class,
+      LockNamespace.class,
+      PrintHelp.class,
+      RunTest.class,
+      UnlockNamespace.class,
+      UploadResults.class
+    };
+    TaskContainer taskContainer = project.getTasks();
+    for (int ctr = 0; ctr < commandClasses.length; ctr++) {
+      Class commandClass = commandClasses[ctr];
+      taskContainer
+          .create(commandClass.getSimpleName(), TestRunnerTask.class)
+          .setMain(commandClass.getCanonicalName());
+    }
+
+    // set the classpath of all tasks to sourceSets.main.runtimeClasspath
+    logger.debug(
+        "Setting the classpath of all TestRunner tasks to sourceSets.main.runtimeClasspath");
+    project.afterEvaluate(
+        p -> {
+          final SourceSetContainer sourceSets =
+              (SourceSetContainer) project.getProperties().get(PROPERTY_NAME_SOURCE_SETS);
+          project
+              .getTasks()
+              .withType(TestRunnerTask.class)
+              .forEach(
+                  testRunnerTask ->
+                      testRunnerTask.setClasspath(
+                          sourceSets.findByName(SOURCE_SET_NAME_MAIN).getRuntimeClasspath()));
+        });
   }
 }
--- src/main/java/bio/terra/testrunner/plugin/TestRunnerPluginExtension.java
+++ src/main/java/bio/terra/testrunner/plugin/TestRunnerPluginExtension.java
@@ -1,40 +1,15 @@
 package bio.terra.testrunner.plugin;
 
 public class TestRunnerPluginExtension {
-  private String targetEnv;
-  private String targetSvc;
-  private String projectId;
-  private String namespace;
+  public static final String EXTENSION_NAME = "testRunner";
 
-  public String getTargetEnv() {
-    return targetEnv;
+  private String SAKeyDirectoryPath;
+
+  public String getSAKeyDirectoryPath() {
+    return SAKeyDirectoryPath;
   }
 
-  public void setTargetEnv(String targetEnv) {
-    this.targetEnv = targetEnv;
-  }
-
-  public String getTargetSvc() {
-    return targetSvc;
-  }
-
-  public void setTargetSvc(String targetSvc) {
-    this.targetSvc = targetSvc;
-  }
-
-  public String getProjectId() {
-    return projectId;
-  }
-
-  public void setProjectId(String projectId) {
-    this.projectId = projectId;
-  }
-
-  public String getNamespace() {
-    return namespace;
-  }
-
-  public void setNamespace(String namespace) {
-    this.namespace = namespace;
+  public void setSAKeyDirectoryPath(String SAKeyDirectoryPath) {
+    this.SAKeyDirectoryPath = SAKeyDirectoryPath;
   }
 }
